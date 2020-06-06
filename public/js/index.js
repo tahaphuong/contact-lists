1ed8d2dd4140b92a02ce69c6336ac1dc2dc416ea
tahaphuong <phuonghata1995@gmail.com>
2020-06-06 20:34:22 +0700

    unclean code

--- public/js/index.js
+++ public/js/index.js
@@ -1,21 +1,13 @@
 window.onload = init
 
-var model = {}
-
-model.listDatabase = []
-
 async function init() {
   // Auth here
-  await controller.getAllItems()
-  view.showListItem()
+  showListItem()
 
   // Delete item
-  view.deleteItem()
 
   // add new 
   let addButton = document.getElementById("add-button")
-  addButton.onclick = view.addNewItem
-
-  //  
+  addButton.onclick = addNewItem
 }
 
--- public/js/view/view.js
+++ public/js/view/view.js
@@ -1,40 +1,71 @@
-let view = {}
+// var controller = {}
+// async function getAllItems() {
+// }
+// controller.addNewItemToServer = async (item, onSuccess, onError) => {
+//   console.log(item)
+//   if (item.hasOwnProperty("name")) {
+//     onSuccess(item)
+//     console.table(item)
+//   } else {
+//     onError("error!")
+//     console.error("error")
+//   }
+// }
 
-view.showListItem = async () => {  
-  let listContacts = document.getElementById("list-contacts")
-  listContacts.innerHTML = ``
+// controller.deleteItem = async (id) => {
+//   console.log(id)
+// }
+async function showListItem() {  
+  const xhr = new XMLHttpRequest()
+  const apiRoute = "api/contact/"
+  xhr.open("GET", apiRoute, true);
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.onreadystatechange = () => {
+    if(xhr.readyState === XMLHttpRequest.DONE) {
+      var status = xhr.status;
+      if (status === 0 || (status >= 200 && status < 400)) {
+        let data = JSON.parse(xhr.responseText)  
+        console.table(data)
 
-  console.log("hello")
-  console.table(model.listDatabase)
+        let listContacts = document.getElementById("list-contacts")
+        listContacts.innerHTML = ``
 
-  for (let item of model.listDatabase) {
-    let contactItem = `
-    <div class="contact-item">
-      <div class="contact-item-avatar">
-        <div></div>
-      </div>
-      <div class="contact-item-info">
-        <div class="contact-item-main-info">${item.name}</div>
-        <div class="contact-item-sub-info">${item.location == null ? "N/A" : item.location}</div>
-      </div>
-      <div class="contact-item-phone">
-        <div>${item.phone}</div>
-      </div>
-      <div class="contact-item-email">
-        <div>${item.email == null ? "N/A" : item.email}</div>
-      </div>
-      <div class="contact-item-action">
-        <ion-icon name="mail-outline"></ion-icon>
-        <ion-icon name="build-outline"></ion-icon>
-        <button deleteObjectId="${item.id}" class="delete-button"><ion-icon style="color: #e96b6b" name="trash-outline"></ion-icon></button>
-      </div>
-    </div>
-    `
-    listContacts.innerHTML += contactItem;
+        for (let item of data) {
+          let contactItem = `
+          <div class="contact-item">
+            <div class="contact-item-avatar">
+              <div></div>
+            </div>
+            <div class="contact-item-info">
+              <div class="contact-item-main-info">${item.name}</div>
+              <div class="contact-item-sub-info">${item.location == null ? "N/A" : item.location}</div>
+            </div>
+            <div class="contact-item-phone">
+              <div>${item.phone}</div>
+            </div>
+            <div class="contact-item-email">
+              <div>${item.email == null ? "N/A" : item.email}</div>
+            </div>
+            <div class="contact-item-action">
+              <ion-icon name="mail-outline"></ion-icon>
+              <ion-icon name="build-outline"></ion-icon>
+              <button deleteobjectid="${item.id}" class="delete-button"><ion-icon style="color: #e96b6b" name="trash-outline"></ion-icon></button>
+            </div>
+          </div>
+          `
+          listContacts.innerHTML += contactItem;
+        }
+        deleteItem()
+      } else {
+        console.error("oh no")
+        return null
+      }
+    }
   }
+  xhr.send();
 }
 
-view.addNewItem = async () => {
+async function addNewItem() {
   let errorMessage = document.getElementById("error-message")
   let form = document.getElementById("contact-form")
   let name = form.name.value
@@ -58,7 +89,8 @@ view.addNewItem = async () => {
   if (!validateData.includes(false)) {
     await controller.addNewItemToServer(objItem, () => {
       console.log("yay")
-      model.listDatabase.push(objItem)
+      console.table(objItem)
+      // model.listDatabase.push(objItem)
     }, () => {
       console.error("oh no")
     })
@@ -71,18 +103,13 @@ view.addNewItem = async () => {
   }
 }
 
-view.deleteItem = () => {
+async function deleteItem() {
   let deleteButtons = document.querySelectorAll(".delete-button");
   for (let deleteButton of deleteButtons) {
-    let itemId = deleteButton.getAttribute("deleteObjectId")
-    deleteButton.onclick = deleteItem
-
-    function deleteItem() {
-      controller.deleteItem(itemId)
+    let itemId = deleteButton.getAttribute("deleteobjectid")
+    deleteButton.onclick = deleteContact
+    function deleteContact() {
+      console.log(itemId)
     }
   } 
-}
-
-view.findItem = () => {
-  
 }
